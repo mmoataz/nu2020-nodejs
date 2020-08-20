9a7f112f225a90d6c6e92255ea64e4262b617884
mmoataz <59367014+mmoataz@users.noreply.github.com>
2020-08-20 17:19:38 +0200

    Add files via upload

---
+++ Day1_JS/test.js
@@ -0,0 +1,14 @@
+// Working with variables
+var x =5,y=10;
+console.log(x);
+console.log(x+y);
+// Working with conditions
+if (x==y){
+    console.log("The two numbers are equal");
+}
+else if (x>y){
+    console.log("X is greater");
+}
+else{
+    console.log("Y is greater");
+}
