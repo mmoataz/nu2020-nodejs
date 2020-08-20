f8cc2423b6a242c61091937440ec9aa9d98ab969
mmoataz <59367014+mmoataz@users.noreply.github.com>
2020-08-20 17:40:32 +0200

    Add files via upload

---
+++ Day1_JS/loops.js
@@ -0,0 +1,11 @@
+let x=10;
+// While loop example for printing numbers from 0 to x-1
+while(x--){
+    console.log(x);
+}
+
+let y =5;
+// for loop example
+for (let i=1; i<=5; i++){
+    console.log("This is round number " +i)
+}
